bbeae43b4a7f6b53c5b0be730e45e5ced4f9b17d
SebasIsd <sdsnt2003@gmail.com>
2025-10-06 18:32:01 -0500

    Trnscipcion de ingles a español y se agrego un carrusel para los desarrolladores

--- js/main.js
+++ js/main.js
@@ -267,5 +267,27 @@
 
         addToCart(itemDetails);
     });
+
+        $('.team-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            }
+        }
+    });
     
 })(jQuery);
